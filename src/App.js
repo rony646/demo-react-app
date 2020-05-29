462322c25fa839c3395b9e066a11ed6c6f6ee949
rony646 <rony646@users.noreply.github.com>
2020-05-28 21:06:49 -0300

    added radium to the app and some styling to the swtich button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
+import Radium from 'radium';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
 
 class App extends Component {
 
@@ -50,7 +51,11 @@ class App extends Component {
         font: 'inherit',
         border: '1px solid blue',
         padding: '8px',
-        cursor: 'pointer'
+        cursor: 'pointer',
+        ':hover': {
+          backgroundColor: 'lightgreen',
+          color: 'black'
+        }
     }
 
     let persons = null;
@@ -71,6 +76,10 @@ class App extends Component {
       )
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     const classes = [];
@@ -131,4 +140,4 @@ class App extends Component {
 
 
 
-export default App;
+export default Radium(App);
